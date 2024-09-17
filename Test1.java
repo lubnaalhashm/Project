2947c152c14ddc0717bf40c9312e06cec9f10316
Lubna <lubnaali7912@gmail.com>
2024-09-17 00:49:50 -0700

    first commit

---
+++ Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+    public static void main(String[] args) {
+        // Loop through numbers from 1 to 20
+        for (int i = 1; i <= 20; i++) {
+            // Check if the number is even
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            }
+        }
+    }
+}
+
